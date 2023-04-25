34391fc9875ba070ff25ffb28742d4d4c52d70a1
kieloBash <114970278+kieloBash@users.noreply.github.com>
2023-04-25 09:29:43 +0800

    ghpages

--- home/src/App.js
+++ home/src/App.js
@@ -1,17 +1,33 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 function App() {
-  const [data, setData] = useState({});
+  const [data, setData] = useState();
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     axios.get("http://localhost:1337/api/get").then((res) => {
-      console.log(res.data);
-      setData(res.data);
+      console.log(res.data.values);
+      setData(res.data.values);
       setLoading(false);
     });
   }, []);
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {loading ? (
+        <></>
+      ) : (
+        <div>
+          {data.map((row, index) => {
+            if (index === 0) {
+              return <div key={index}></div>;
+            } else {
+              return <div key={index}>{row[0]}</div>;
+            }
+          })}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
