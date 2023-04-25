b9d1f36390e1a1f28726073fa2036de69a2f6790
kieloBash <114970278+kieloBash@users.noreply.github.com>
2023-04-25 09:37:56 +0800

    added fileUpload of excel

--- home/src/App.js
+++ home/src/App.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import * as XLSX from "xlsx";
+
 function App() {
   const [data, setData] = useState();
   const [loading, setLoading] = useState(true);
@@ -11,6 +13,38 @@ function App() {
       setLoading(false);
     });
   }, []);
+
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileSelect = (event) => {
+    let file = event.target.files[0];
+    setSelectedFile(file);
+
+    const promise = new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsArrayBuffer(file);
+
+      fileReader.onload = (e) => {
+        const bufferArray = e.target.result;
+
+        const wb = XLSX.read(bufferArray, { type: "buffer" });
+
+        const wsname = wb.SheetNames[0];
+        const ws = wb.Sheets[wsname];
+        const data = XLSX.utils.sheet_to_json(ws);
+        resolve(data);
+      };
+
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+
+    promise.then((d) => {
+      console.log(d);
+    });
+  };
+
   return (
     <div className="App">
       {loading ? (
@@ -24,6 +58,7 @@ function App() {
               return <div key={index}>{row[0]}</div>;
             }
           })}
+          <input type="file" onChange={handleFileSelect}></input>
         </div>
       )}
     </div>
