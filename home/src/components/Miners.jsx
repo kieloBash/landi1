524f7f0402f84c062f1629726e23a660aa7c859c
Kielo Bash Mercado <kielo_bash_mercado@dlsu.edu.ph>
2023-05-01 12:09:14 +0800

    added search function

--- home/src/components/Miners.jsx
+++ home/src/components/Miners.jsx
@@ -1,13 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Miners = ({ miners, toggleCompute }) => {
+  const [searchMiners, setSearchMiners] = useState(miners);
+  const [search, setsearch] = useState("");
+
+  useEffect(() => {
+    if (search !== "") {
+      let temp = [];
+      miners.forEach((val) => {
+        if (val.toLowerCase() === search.toLowerCase()) {
+          temp.push(val);
+        }
+        setSearchMiners(temp);
+      });
+    } else {
+      setSearchMiners(miners);
+    }
+  }, [search,miners]);
+
   return (
     <div className="relative w-full p-4 overflow-hidden bg-white shadow-lg rounded-xl md:w-60 dark:bg-gray-800">
       <p className="mb-6 text-xl text-center font-medium text-gray-600 dark:text-white">
         Miners Today
       </p>
+      <div className="max-w-md mx-auto">
+        <div className="relative flex items-center w-[11rem] h-10 rounded-lg focus-within:shadow-lg bg-white overflow-hidden mb-4">
+          <input
+            className="peer h-full w-full outline-none text-sm text-pink-400 pl-2"
+            type="text"
+            id="search"
+            placeholder="Search Miner..."
+            onChange={(e) => setsearch(e.target.value)}
+          />
+          {/* <div className="grid place-items-center h-full w-12 text-pink-400">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-6 h-6"
+              onClick={handleSearch}
+            >
+              <path
+                fillRule="evenodd"
+                d="M10.5 3.75a6.75 6.75 0 100 13.5 6.75 6.75 0 000-13.5zM2.25 10.5a8.25 8.25 0 1114.59 5.28l4.69 4.69a.75.75 0 11-1.06 1.06l-4.69-4.69A8.25 8.25 0 012.25 10.5z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div> */}
+        </div>
+      </div>
       <div className="grid grid-cols-3 gap-4 max-h-[10rem] w-[11rem] overflow-y-auto overflow-x-hidden">
-        {miners.map((miner, index) => {
+        {searchMiners.map((miner, index) => {
           const min = 1; // minimum value
           const max = 5; // maximum value
           const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
@@ -36,7 +79,10 @@ const Miners = ({ miners, toggleCompute }) => {
           return (
             <div className="flex flex-col items-center" key={index}>
               <div className="relative">
-                <button className="relative block" onClick={()=>toggleCompute(miner)}>
+                <button
+                  className="relative block"
+                  onClick={() => toggleCompute(miner)}
+                >
                   <div
                     className={`mx-auto object-cover rounded-full h-10 w-10 ${color}`}
                   ></div>
--- home/src/pages/Mobile.jsx
+++ home/src/pages/Mobile.jsx
@@ -9,7 +9,7 @@ import CopiedPrompt from "../components/CopiedPrompt";
 import AddMinerMobile from "../components/AddMinerMobile";
 
 import * as XLSX from "xlsx";
-import { saveAs } from 'file-saver';
+import { saveAs } from "file-saver";
 
 const Mobile = () => {
   const [data, setData] = useState([]);
@@ -28,7 +28,7 @@ const Mobile = () => {
     let temp = [];
     let tempTotal = 0;
     let tempMiners = [];
-    
+
     for (let i = 0; i < data.length; i++) {
       if (i !== 0) {
         tempMiners.push(data[i].Recipient);
@@ -127,55 +127,61 @@ const Mobile = () => {
 
   const saveAsExcel = (buffer, filename) => {
     const datas = new Blob([buffer], { type: EXCEL_TYPE });
-    saveAs(datas,filename+'_export_'+new Date().getTime()+'.xlsx');
+    saveAs(datas, filename + "_export_" + new Date().getTime() + ".xlsx");
   };
   return (
     <div className="App flex justify-center items-center bg-pink-300 w-screen h-screen">
-      {loading && data ? (
+      {loading ? (
         <div>
           <Hero toggleMobileData={toggleMobileData} />
         </div>
       ) : (
-        <div className="flex w-full justify-evenly items-center flex-col md:flex-row h-full">
-          <div className="grid grid-cols-1 gap-y-4">
-            <TotalPrice total={total} no_items={no_items} />
-          </div>
-          <div>
-            <Miners miners={miners} toggleCompute={toggleCompute} />
-          </div>
-          <div className="flex">
-            <AddMinerMobile handleAdd={handleAdd} miners={miners}/>
+        <>
+          {data.length > 0 && miners.length > 0 ? (
+            <div className="flex w-full justify-evenly items-center flex-col md:flex-row h-full">
+              <div className="grid grid-cols-1 gap-y-4">
+                <TotalPrice total={total} no_items={no_items} />
+              </div>
+              <div>
+                <Miners miners={miners} toggleCompute={toggleCompute} />
+              </div>
+              <div className="flex">
+                <AddMinerMobile handleAdd={handleAdd} miners={miners} />
 
-            <button
-              type="button"
-              onClick={downloadAsExcel}
-              className="ml-2 py-2 px-4 flex justify-center items-center  bg-pink-600 hover:bg-pink-700 focus:ring-pink-500 focus:ring-offset-pink-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  w-12 h-12 rounded-lg "
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M12 2.25a.75.75 0 01.75.75v11.69l3.22-3.22a.75.75 0 111.06 1.06l-4.5 4.5a.75.75 0 01-1.06 0l-4.5-4.5a.75.75 0 111.06-1.06l3.22 3.22V3a.75.75 0 01.75-.75zm-9 13.5a.75.75 0 01.75.75v2.25a1.5 1.5 0 001.5 1.5h13.5a1.5 1.5 0 001.5-1.5V16.5a.75.75 0 011.5 0v2.25a3 3 0 01-3 3H5.25a3 3 0 01-3-3V16.5a.75.75 0 01.75-.75z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </button>
+                <button
+                  type="button"
+                  onClick={downloadAsExcel}
+                  className="ml-2 py-2 px-4 flex justify-center items-center  bg-pink-600 hover:bg-pink-700 focus:ring-pink-500 focus:ring-offset-pink-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  w-12 h-12 rounded-lg "
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M12 2.25a.75.75 0 01.75.75v11.69l3.22-3.22a.75.75 0 111.06 1.06l-4.5 4.5a.75.75 0 01-1.06 0l-4.5-4.5a.75.75 0 111.06-1.06l3.22 3.22V3a.75.75 0 01.75-.75zm-9 13.5a.75.75 0 01.75.75v2.25a1.5 1.5 0 001.5 1.5h13.5a1.5 1.5 0 001.5-1.5V16.5a.75.75 0 011.5 0v2.25a3 3 0 01-3 3H5.25a3 3 0 01-3-3V16.5a.75.75 0 01.75-.75z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </button>
 
-            {/* <MineNumberComponent mineNumbers={mineNumbers} /> */}
-          </div>
+                {/* <MineNumberComponent mineNumbers={mineNumbers} /> */}
+              </div>
 
-          {copied ? (
-            <div className="absolute h-screen w-screen flex justify-center items-center bg-pink-300">
-              <CopiedPrompt toggleClose={toggleClose} />
+              {copied ? (
+                <div className="absolute h-screen w-screen flex justify-center items-center bg-pink-300">
+                  <CopiedPrompt toggleClose={toggleClose} />
+                </div>
+              ) : (
+                <></>
+              )}
             </div>
           ) : (
             <></>
           )}
-        </div>
+        </>
       )}
     </div>
   );
